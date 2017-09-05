000f7a9d5803469d95a3f80889967fc670a958af
Matt Stuhring <whereyouatmatt@gmail.com>
2017-09-05 11:33:48 -0700

    prepare for heroku

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { browserHistory, withRouter } from 'react-router';
-import {Jumbotron, Button} from 'react-bootstrap';
+import {Jumbotron, Button, Image} from 'react-bootstrap';
 import { Link } from 'react-router'
 
 export class Home extends React.Component {
@@ -9,12 +9,12 @@ export class Home extends React.Component {
     return (
       <div className="row home">
         <div className="col-sm-12">
-          <Jumbotron>
-            <h2>NEVER DO LAUNDRY AGAIN</h2>
+          <div className="welcome">
+            <h1>NEVER DO YOUR LAUNDRY AGAIN</h1>
             <Link to="/login">
-              <Button bsStyle="primary" bsSize="large">Get Started</Button>
-            </Link>
-          </Jumbotron>
+            <Button bsStyle="primary" bsSize="large">Get Started</Button>
+          </Link>
+          </div>
         </div>
       </div>
     )
