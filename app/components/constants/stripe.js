3562324dc0e5944f49d83e926f9762d10603d776
Matt Stuhring <whereyouatmatt@gmail.com>
2017-09-06 18:06:22 -0700

    fix production stripe key error

--- app/components/constants/stripe.js
+++ app/components/constants/stripe.js
@@ -1,5 +1,7 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_MY_PUBLISHABLE_KEY'
-  : 'pk_test_TwpxwIMEGHIr1gCLTNRuw8Wy';
+// const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
+//   ? 'pk_live_MY_PUBLISHABLE_KEY'
+//   : 'pk_test_TwpxwIMEGHIr1gCLTNRuw8Wy';
+
+const STRIPE_PUBLISHABLE = 'pk_test_TwpxwIMEGHIr1gCLTNRuw8Wy';
 
 export default STRIPE_PUBLISHABLE;
